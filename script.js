0318a8401f80ef9db494fd88f3edf7a09d9ea7e6
valeria <valeria@MacBook-Air-yuriy.local>
2023-07-06 14:10:32 +0300

    commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// -----------------1-------------------
+
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
+
+// -----------------2-------------------
+
+let DaysInMonth = 30;
+let HoursInMonth = DaysInMonth * 24;
+let MinutesInMonth = HoursInMonth * 60;
+console.log(HoursInMonth + " hours");
+console.log(MinutesInMonth + " minutes");
+
+// -----------------3-------------------
+
+let helth = 100;
+let energy = 100;
+
+
+console.log(helth-10);
+console.log(energy-10);
+
+// --------or--------
+
+helth -=10;
+energy -=10;
+console.log("helth: " + helth);
+console.log("energy: " + energy);
+
+// -----------------4-------------------
+
+let total = 1500;
+let discount = total * 10 / 100;
+console.log("To pay:", + total-discount);
+
+// -----------------5-------------------
+
+let number = 15.792;
+console.log(Math.floor(number));
+
+// -----------------6-------------------
+
+let string1 = "7.4";
+console.log(parseFloat(string1));
+
+// -----------------7-------------------
+
+let string2 = "25";
+console.log(parseInt(string2));
+
+// -----------------8-------------------
+
+let number2 = 49;
+console.log(Math.sqrt(number2));
+
+// -----------------9-------------------
+
+let number3 = 25;
+let string3 = "15.3";
+
+console.log(parseInt(string3));
+console.log(number3.toString());
